083f2b423cc2f55ef583ab8b2a11959b099cf53b
LaupWing <loc_strict@hotmail.com>
2020-01-08 17:43:47 +0100

    added logger middleware

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux'
-import {createStore, combineReducers} from  'redux'
+import {createStore, combineReducers, applyMiddleware} from  'redux'
 
 // import reducer from  './store/reducer'
 import counterReducer from './store/reducers/counter'
@@ -15,7 +15,18 @@ const rootReducer = combineReducers({
     res: resultReducer
 })
 
-const store = createStore(rootReducer)
+const logger = store => {
+    return next =>{
+        return action =>{
+            console.log('[Middleware] dispatching', action)
+            const result = next(action)
+            console.log('[Middleware] next state', store.getState())
+            return result
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
